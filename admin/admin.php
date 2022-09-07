6d2dfd17b9e4ec9f6697318bc8ca78731257039c
Shino <shachcha01@gmail.com>
2022-09-07 23:05:27 +0600

    admin panel

---
+++ admin/admin.php
@@ -0,0 +1,64 @@
+<html>
+
+<head>
+    <title>ZetaMod panel</title>
+    <link href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css" rel="stylesheet" />
+    <link rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.1.2/css/fontawesome.min.css" />
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+
+<body>
+    <!--menu section start-->
+    <div class="menu text-align-center">
+        <div class="wrapper container">
+            <ul>
+               <li><a href="#"><i class="bx bx-home"></i></a></li>
+               <li><a href="#"><i class='bx bx-briefcase'></i></a></li>
+               <li><a href="#"><i class='bx bx-book-content'></i></a></li>
+               <li><a href="#"><i class='bx bx-game'></i></a></li>
+               <li><a href="#"><i class='bx bx-chat'></i></a></li> 
+               <li><a href="#"><i class='bx bx-log-out-circle'></i></a></li>
+            </ul>
+        </div>
+
+    </div>
+    <!--menu section end-->
+    <!--main content start-->
+    <div class="main-content">
+        <div class="wrapper container text-align-center">
+            <h1><strong>DASHBOARD</strong></h1>
+            <div class="col-4">
+                <h1>5</h1>
+                 Catagories
+            </div>
+            <div class="col-4">
+                <h1>5</h1>
+                 games
+            </div>
+            <div class="col-4">
+                <h1>5</h1>
+                 requests
+            </div>
+            <div class="col-4">
+                <h1>5</h1>
+                 Catagories
+            </div>
+            <div class="clearfix"></div>
+        </div>
+        
+    </div>
+    <!--main content ends-->
+
+
+    <!--footer section-->
+    <div class="footer">
+        <div class="wrapper container text-align-center">
+        <p>All rights reserverd, Designed By <a href="https://github.com/Mickyx73">Mickyx73</a>  and <a href="https://github.com/uchhasbarua">Uchhasbarua</a> </p>
+        </div>
+    </div>
+    <!--footer Section-->
+
+</body>
+
+</html>
